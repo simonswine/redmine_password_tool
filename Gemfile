5b4fb2953a164b45e054a3f445e6d02c392389a6
Christian Simon <simon@swine.de>
2015-02-24 10:34:21 +0100

    Upgrades testing to 3.2 rspec

--- Gemfile
+++ Gemfile
@@ -4,8 +4,9 @@ gem 'attr_encryptor', '~> 2.0.0'
 gem 'awesome_nested_set'
 
 group :test do
-    gem 'rspec'
-    gem 'rspec-rails'
+    gem 'rspec', '~> 3.2.0'
+    gem 'rspec-rails', '~> 3.2.1'
+    gem 'shoulda'
     gem 'rspec_junit_formatter'
 end
 	
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -11,9 +10,8 @@ Dir[Rails.root.join("plugins/redmine_password_tool/spec/support/**/*.rb")].each 
 RSpec.configure do |config|
   # ## Mock Framework
   #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  #config.mock_with :mocha
+  # Backwards compatability
+  config.expect_with(:rspec) { |c| c.syntax = :should }
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
